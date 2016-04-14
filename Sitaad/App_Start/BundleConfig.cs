ecced620f8105c836c5a8fccd6f8acbd94de5b8c
Saeid <sdsaati@gmail.com>
2016-04-14 19:01:56 +0430

    ASPLayout

--- Sitaad/App_Start/BundleConfig.cs
+++ Sitaad/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace Sitaad
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+            BundleTable.EnableOptimizations = true;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -26,6 +28,59 @@ namespace Sitaad
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            /* Styles for Sitaad Theme */
+            bundles.Add(new StyleBundle("~/Sitaad/GlobalCSS").Include(
+                      "~/Content/Sitaad/plugins/bootstrap/css/bootstrap-rtll.css",
+                      "~/Content/Sitaad/css/css-rtl/ie8-rtl.css",
+                      "~/Content/Sitaad/css/css-rtl/blocks-rtl.css",
+                      "~/Content/Sitaad/css/css-rtl/plugins-rtl.css",
+                      "~/Content/Sitaad/css/css-rtl/app-rtl.css",
+                      "~/Content/Sitaad/css/css-rtl/style-rtl.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/HeaderFooterCSS").Include(
+                      "~/Content/Sitaad/css/css-rtl/headers/header-default-rtl.css",
+                      "~/Content/Sitaad/css/css-rtl/footers/footer-v1-rtl.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/ImplementingPluginsCSS").Include(
+                      "~/Content/Sitaad/plugins/animate.css",
+                      "~/Content/Sitaad/plugins/line-icons/line-icons.css",
+                      "~/Content/Sitaad/plugins/font-awesome/css/font-awesomee.css",
+                      "~/Content/Sitaad/plugins/fancybox/source/jquery.fancybox.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/owlCSS").Include(
+                      "~/Content/Sitaad/plugins/owl-carousel2/assets/owl.carousel.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/IE9CSS").Include(
+                      "~/Content/Sitaad/plugins/revolution-slider/rs-plugin/css/settings.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/StyleSwitcherCSS").Include(
+                      "~/Content/Sitaad/plugins/revolution-slider/rs-plugin/css/settings-ie8.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/TurbosoftsCSS").Include(
+                      "~/Content/Sitaad/css/Turbosofts.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/ThemeCSS").Include(
+                      "~/Content/Sitaad/css/css-rtl/theme-colors/Sitadblue.css",
+                      "~/Content/Sitaad/css/css-rtl/theme-skins/dark.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/RTLCustomizationCSS").Include(
+                      "~/Content/Sitaad/css/css-rtl/rtl.css"));
+            bundles.Add(new StyleBundle("~/Sitaad/CustomizationCSS").Include(
+                      "~/Content/Sitaad/css/css-rtl/custom-rtl.css"));
+            /* Scripts for Sitaad Theme */
+            bundles.Add(new ScriptBundle("~/Sitaad/GlobalJS").Include(
+                            "~/Content/Sitaad/plugins/jquery/jquery.js",
+                            "~/Content/Sitaad/plugins/jquery/jquery-migratee.js",
+                            "~/Content/Sitaad/plugins/bootstrap/js/bootstrapp.js"));
+            bundles.Add(new ScriptBundle("~/Sitaad/ImplementingJS").Include(
+                            "~/Content/Sitaad/plugins/back-to-top.js",
+                            "~/Content/Sitaad/plugins/jquery.parallax.js",
+                            "~/Content/Sitaad/plugins/fancybox/source/jquery.fancybox.pack.js",
+                            "~/Content/Sitaad/plugins/revolution-slider/rs-plugin/js/jquery.themepunch.toolss.js",
+                            "~/Content/Sitaad/plugins/revolution-slider/rs-plugin/js/jquery.themepunch.revolutionn.js"));
+            bundles.Add(new ScriptBundle("~/Sitaad/PageLevelJS").Include(
+                            "~/Content/Sitaad/js/app.js",
+                            "~/Content/Sitaad/js/plugins/fancy-box.js",
+                            "~/Content/Sitaad/js/plugins/revolution-slider.js"));
+            bundles.Add(new ScriptBundle("~/Sitaad/CustomizationJS").Include(
+                            "~/Content/Sitaad/js/custom.js"));
+            bundles.Add(new ScriptBundle("~/Sitaad/IE9JS").Include(
+                            "~/Content/Sitaad/plugins/respond.js",
+                            "~/Content/Sitaad/plugins/html5shiv.js",
+                            "~/Content/Sitaad/plugins/placeholder-IE-fixes.js"));
         }
+        
     }
 }
